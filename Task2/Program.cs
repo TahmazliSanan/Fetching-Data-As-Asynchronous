2118f1eb6c979436ddb383c1ff2560c0f2da9e48
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-12-17 02:52:44 +0400

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Task2
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var task = await FetchData("https://catfact.ninja/fact");
+            Console.WriteLine(task?.Fact);
+            Console.ReadLine();
+        }
+
+        public static async Task<CatFact?> FetchData(string apiUrl)
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(apiUrl);
+            var catFact = JsonConvert.DeserializeObject<CatFact>(response);
+            return catFact;
+        }
+    }
+}
